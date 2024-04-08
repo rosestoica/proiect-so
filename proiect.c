058161d8c641d13aea13349001f19216c11e6429
rose <rose@rose.rose>
2024-04-08 11:45:13 -0400

    first part

---
+++ proiect.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+void error(char * msg)
+{
+  perror(msg);
+  exit(-1);
+}
+
+
+
+
+void parse(const char * dir_path)
+{
+  DIR * d=NULL;
+  if((d=opendir(dir_path))== NULL)
+    error("eroare deschidere director");
+
+  
+  struct dirent * curent=NULL;
+  while((curent=readdir(d))!=NULL)
+    {
+
+      //creeare cale realativa
+      // printf("%s \n",curent->d_name);
+      char cale_relativa[256];
+      strcpy(cale_relativa,dir_path);
+      int n=strlen(cale_relativa);
+      cale_relativa[n]='/';
+      cale_relativa[n+1]=0;
+      strcat(cale_relativa, curent->d_name);
+      // printf("%s \n",cale_relativa);
+
+
+      struct stat status;
+      lstat(cale_relativa, &status);
+
+      if(S_ISDIR(status.st_mode))
+	{
+	  n=strlen(cale_relativa);
+	  if(cale_relativa[n-1]!='.')
+	    parse(cale_relativa);
+	}
+      else
+	{ //prelucrare fisier
+	  printf("%s %ld %ld  \n", cale_relativa, status.st_ino,status.st_size);
+
+	}
+
+      
+
+
+
+      
+    }
+
+  
+  if(closedir(d)!=0)
+    error("eroare la inchidere");
+
+
+}
+
+
+int main(int argc , char ** argv)
+{
+  if(argc<2)
+    error("argumente insuficiente");
+  parse(argv[1]);
+
+  return 0;
+}
